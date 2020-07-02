952b1cd6a8e1b788f21a1c2361b4472c13b4340c
Edaz <edazpotato@gmail.com>
2020-07-03 09:46:40 +1200

    Create cookie-consent.js

---
+++ cookie-consent.js
@@ -0,0 +1,19 @@
+function showCookieBanner() {
+	var cssStr = "\
+#cookie-banner {position: fixed;bottom: 40px;left: 10%;right: 10%;width: 80%;padding: 5px 14px;display: flex;align-items: center;justify-content: space-between;flex-direction: row;background-color: #eee;border-radius: 5px;box-shadow: 0 0 2px 1px rgba(0, 0, 0, 0.2);}\
+#cookie-close {height: 20px;background-color: #777;border: none;color: white;border-radius: 2px; cursor: pointer;align-self: flex-end;}";
+  	var styleEl = document.createElement("style");
+  	styleEl.innerHTML = cssStr;
+  	document.body.appendChild(styleEl);
+	var htmlStr = '<div id="cookie-banner" style="display: block"><p>By continuing using this website, you consent to us and various third party providers using cookies and other related technolgies, which save data in your browser</p><button id="cookie-close">&times;</button></div>';
+  	var el = document.createElement("div");
+  	el.innerHTML = htmlStr;
+  	document.body.appendChild(el);
+  	document.getElementById("cookie-close").addEventListener("click", function() {
+    	el.setAttribute("style", "display: none;");
+  	});
+}
+if (localStorage.getItem('cookieSeen') !== 'shown') {
+  //localStorage.setItem('cookieSeen','shown')
+  showCookieBanner();
+}
